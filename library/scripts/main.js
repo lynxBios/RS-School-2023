4c1fee07f26d4c29c3cd4edaf4b2d68d04567c96
lynxBios <lregvjlke@gmail.com>
2023-08-09 12:41:33 +0300

    feat: implement hide menu-burger on click on menu item

--- library/scripts/main.js
+++ library/scripts/main.js
@@ -2,12 +2,19 @@
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__navigation');
-    const menuCloseItem = document.querySelector('.header__nav-close')
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__navigation-active');
         
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__navigation-active');
-    })
+    });
+    
+    for (let i = 0; i < menuLinks.length; i += 1) {
+        menuLinks[i].addEventListener('click', () => {
+            menu.classList.remove('header__navigation-active');
+        });
+    }
 }());
