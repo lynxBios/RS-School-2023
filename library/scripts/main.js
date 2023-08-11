f2da11a137a0feead4425753d317891d798ae51a
lynxBios <lregvjlke@gmail.com>
2023-08-11 14:22:47 +0300

    feat: the adaptive menu is smoothly hidden, leaving the screen on click on the area outside the menu

--- library/scripts/main.js
+++ library/scripts/main.js
@@ -3,12 +3,16 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__navigation');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuCloseMain = document.querySelector('.main');
     const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__navigation-active');
         
     });
     menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__navigation-active');
+    });
+    menuCloseMain.addEventListener('click', () => {
         menu.classList.remove('header__navigation-active');
     });
     
