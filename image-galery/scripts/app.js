09d23d42d1cf2a8936cd7ef789f06f337d182f58
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-10-01 20:31:44 +0300

    feat: implement default layout of image galery

--- image-galery/scripts/app.js
+++ image-galery/scripts/app.js
@@ -1,25 +1,50 @@
 'use strict';
 
-const button = document.querySelector('.search__btn');
-const image = document.querySelector('.image');
-const url = 'https://api.unsplash.com/photos/?client_id=C7MyJoXDDp8cWtTxLstCqMQxOHYalhZ6iZPm4UV3ZLM';
+const buttonGo = document.querySelector('.search__btn');
 
-const searchResults = document.querySelector('.search-results');
+const url = 'https://api.unsplash.com/photos/random?query=spring&per_page=30&orientation=landscape&client_id=C7MyJoXDDp8cWtTxLstCqMQxOHYalhZ6iZPm4UV3ZLM';
+const defaultQuery = 'ocean';
+const imagesGrid = document.querySelector('.images__grid');
+const searchWord = document.getElementById('srch_word');
 
-
-async function fetchImages() {
-  try {
-    const response = await fetch(url);
+async function fetchImages(query = defaultQuery) {
+ 
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=C7MyJoXDDp8cWtTxLstCqMQxOHYalhZ6iZPm4UV3ZLM`);
     const data = await response.json();
-
-    image.src = data.file
-    console.log(data)
-  } catch (error) {
-    console.log(error);
-  }
+    showImages(data);    
   
 }
-button.addEventListener('click', () => {
+
+function showImages(data) {
+  
+  data.results.forEach(image => {
+    const picture = document.createElement('img');
+    picture.classList.add('img');
+    picture.src = image.urls.regular;
+    imagesGrid.appendChild(picture);
+  });
+}
+
+
+
+
+buttonGo.addEventListener('click', () => {
   fetchImages();
 });
+
+searchWord.addEventListener('keydown', pressEnter)
+
+function pressEnter(e) {
+  if (e.keyCode === 13) {
+    fetchImages();
+  }
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  const inputElement = document.getElementById('srch_word');
+  if(inputElement) {
+      inputElement.focus();
+  }
+});
+
   
