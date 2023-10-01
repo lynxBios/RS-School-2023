a8dfac295531b942c32c6914bb15c2b09cbfcd04
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-10-01 16:56:46 +0300

    feat: add search-close button

--- image-galery/scripts/app.js
+++ image-galery/scripts/app.js
@@ -1,28 +1,25 @@
 'use strict';
 
-const searchForm = document.getElementById('search-form');
-const searchInput = searchForm.querySelector('[type="search"]');
+const button = document.querySelector('.search__btn');
+const image = document.querySelector('.image');
+const url = 'https://api.unsplash.com/photos/?client_id=C7MyJoXDDp8cWtTxLstCqMQxOHYalhZ6iZPm4UV3ZLM';
+
 const searchResults = document.querySelector('.search-results');
 
 
-// напишем обработчик события ввода с клавиатуры в поле поиска
-searchInput.addEventListener('input', (e) => {
-    // Получаем значение в поле,
-    // на котором сработало событие:
-    const { value } = e.target
+async function fetchImages() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    image.src = data.file
+    console.log(data)
+  } catch (error) {
+    console.log(error);
+  }
   
-    // Получаем список imgs от сервера:
-    server.search(value).then(function (response) {
-      const { list } = response
-  
-      // Проходим по каждому из элементов списка,
-      // и составляем строчку с несколькими <li> элементами...
-      const html = list.reduce((markup, item) => {
-        return `${markup}<li>${item}</li>`
-      }, ``)
-  
-      // ...которую потом используем как содержимое списка:
-      searchResults.innerHTML = html
-    })
-  })
+}
+button.addEventListener('click', () => {
+  fetchImages();
+});
   
